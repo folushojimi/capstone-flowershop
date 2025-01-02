625e810491d367cf00cf9126783088adc61c5d56
folushojimi <iamrblogger@gmail.com>
2025-01-02 18:09:03 +0100

    fields component created

--- src/pages/auth/AuthForm/fields.jsx
+++ src/pages/auth/AuthForm/fields.jsx
@@ -1,8 +1,21 @@
-const Fields =()=>{
+const Fields =({label, type,  onChange, value})=>{
+
     return(
         <>
-        
-        
+<div className="flex flex-col my-4"> 
+
+<label htmlFor={label}>
+ {label}
+</label>
+
+<input 
+ id={label}
+ type={type}
+ value={value}
+ onChange={onChange}
+ className="w-64 px-2 py1 border bg-slate-50 border-slate-200 rounded-lg focus:outline-emerald-600"
+/>
+</div>
         </>
     )
 }
--- src/pages/auth/AuthForm/index.jsx
+++ src/pages/auth/AuthForm/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Fields from "./fields";
 
 const AuthForm =({fields, submitButtonLabel})=> {
 
@@ -11,40 +12,29 @@ const AuthForm =({fields, submitButtonLabel})=> {
             initialState[field.label] = '';
         }
 
+        
+
         return initialState
+
     });
-
-   
-
-
- 
-
-
-
+    console.log(values)
 
     return(
      <>
         <form className="font-lato bg-white border rounded-lg border-slate-300 m-4 p-4">
             {
 
-            fields.map((field)=> <div key={field.label} className="flex flex-col my-4">
-
-             <label htmlFor={field.label}>
-              {field.label}
-            </label>
-
-            <input 
-              id={field.label}
-              type={field.type}
-              value={values[fields.label]}
-              onChange={e=> {
-                setValues({ ...values, [field.label]: e.target.value});
-              }}
-              className="w-64 px-2 py1 border bg-slate-50 border-slate-200 rounded-lg focus:outline-emerald-600"
+            fields.map((field)=> 
+            <Fields 
+            key={field.label}
+            label={field.label}
+            type={field.type}
+            value={values[field.label]}
+            onChange={e=> {
+              setValues({ ...values, [field.label]: e.target.value});
+            }}
             />
-            </div>)
-
-            }
+            )}
 
             <button className="text-white bg-emerald-700 w-full py-2 rounded-lg mt-4">
             {submitButtonLabel}
